e11d296394ecd7c445afd6eb5e101c409e972f3d
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-08 23:30:32 +0000

    added skills section and work, education sections too, initial design of contact page

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     BtnContactOnClick();
+    HideExtraInformation();
 
 })
 
@@ -24,4 +25,45 @@ function SidebarTabOnChange(event)
     document.querySelector(".tab-item.active").classList.remove("active");
     //add active to current
     event.target.classList.add('active');
+}
+
+function ShowExtraInformation()
+{
+    document.querySelector(".main").classList.add("main-container-expanded");
+    document.querySelector(".leftbar-extra-information").style.visibility = "visible";
+}
+
+function HideExtraInformation()
+{
+    document.querySelector(".main").classList.remove("main-container-expanded")
+    document.querySelector(".main").classList.add(".main-container");
+    document.querySelector(".leftbar-extra-information").style.visibility = "hidden";
+}
+
+class Person
+{
+    constructor(fName, lName, age)
+    {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.age = age;
+    }
+
+    GetFullName()
+    {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+function AddLine(obj)
+{
+    var param = document.createElement("p");
+    var textNode = document.createTextNode("Yo");
+
+    param.appendChild(textNode);
+    obj.appendChild(param);
+}
+function DeleteLine(obj) {
+    var elements = obj.getElementsByTagName("p");
+    obj.removeChild(elements[elements.length -1]);
 }
